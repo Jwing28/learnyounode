f27d366e4a1b95a5f4d6818589f1a3b4dea124e3
Jwing28 <jonathan.wing.lee@gmail.com>
2016-07-03 15:17:43 -0700

    [add] tcpServer

---
+++ tcpServer/program.js
@@ -0,0 +1,18 @@
+//Create a TCP time server.
+//no http 
+
+//listen on provided port
+//net method creates server to listen to events
+//when event occurs we are returned the socket object
+//which can be used to write to or end
+//we end with the requested output of time and date
+
+//we use the Date object to create the requested formatting
+var net = require('net');  
+var server = net.createServer(function (socket) {  
+// socket handling logic  
+var date = new Date();
+var fulldate = date.getFullYear() + "-" + "0" + (date.getMonth() + 1) + "-"+ "0" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes() + "\n";
+    socket.end(fulldate);
+});  
+server.listen(process.argv[2]);
